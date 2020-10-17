42f4e4e771ca8cddcf98c6f8b3c2a031617b3baa
vc8006 <vedchourasia08@gmail.com>
2020-10-17 11:40:11 +0530

    second commit by vedant

--- internfair/urls.py
+++ internfair/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('register', views.register, name='register'),
+    path('recruiterLanding', views.recruiterLanding, name='recruiterLanding'),
+    path('recruiterRegistration', views.recruiterRegistration, name='recruiterRegistration'),
+    path('studentProfile1', views.studentProfile1, name='studentProfile1'),
+    path('availableInternships', views.availableInternships, name='availableInternships'),
 ]
--- internfair/views.py
+++ internfair/views.py
@@ -5,6 +5,21 @@ from django.views import View
 def index(request):
     return render(request, "StudentLanding.html")
 
+def register(request):
+    return render(request, "StudentRegistration.html")
+
+def recruiterLanding(request):
+    return render(request, "RecruiterLanding.html")
+
+def recruiterRegistration(request):
+    return render(request, "RecruiterRegistration.html")
+
+def studentProfile1(request):
+    return render(request, "StudentProfile1.html")
+
+def availableInternships(request):
+    return render(request, "AvailableInternships.html")
+
 
 
 
