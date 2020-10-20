cfb8b26933bf577e32b9092da6fabbfea8b9bba2
shyamodule <70153993+shyamodule@users.noreply.github.com>
2020-10-20 19:00:02 +0530

    Update views.py

--- internfair/views.py
+++ internfair/views.py
@@ -5,6 +5,28 @@ from django.views import View
 def index(request):
     return render(request, "StudentLanding.html")
 
+def StudentRegistration(request):
+	return render(request, "StudentRegistration.html")
 
+def StudentProfile(request):
+	return render(request, "StudentProfile1.html")
+
+def AvailableInternships(request):
+	return render(request, "AvailableInternships.html")
+
+
+
+
+def RecruiterLanding(request):
+	return render(request, "RecruiterLanding.html")
+
+def RecruiterRegistration(request):
+	return render(request, "RecruiterRegistration.html")
+
+def AvailableInterns(request):
+	return render(request, "AvailableInterns.html")
+
+def CompanyProfile(request):
+	return render(request, "CompanyProfile.html")
 
 
