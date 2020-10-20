ebe4d694dc8d081b3038e06fe62965f44ed4f6ca
shyamodule <70153993+shyamodule@users.noreply.github.com>
2020-10-20 18:59:18 +0530

    Update urls.py

--- internfair/urls.py
+++ internfair/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('student/register', views.StudentRegistration, name="StudentRegistration"),
+    path('student/profile', views.StudentProfile, name="StudentProfile"),
+    path('student/availableInternships', views.AvailableInternships, name="AvailableInternships"),
+    path('recruiter/', views.RecruiterLanding, name="RecruiterLanding"),
+    path('recruiter/register', views.RecruiterRegistration, name="RecruiterRegistration"),
+    path('recruiter/availableInterns', views.AvailableInterns, name="AvailableInterns"),
+    path('recruiter/companyProfile', views.CompanyProfile, name="CompanyProfile"),
 ]
